60da097ce3721bc2b9c310285ee2a1c40f3ff9d5
zinirun <heojj97@naver.com>
2020-09-07 15:51:50 +0900

    Finish v0.1 + Support 2 items

--- popup.js
+++ popup.js
@@ -1,10 +1,15 @@
-const scanBt = document.querySelector('#clms_scan_bt');
+const scanOneBt = document.querySelector('#clms_scan_one_bt');
+const scanTwoBt = document.querySelector('#clms_scan_two_bt');
 const videoBt = document.querySelector('#video_download_bt');
 const view = document.querySelector('#wrapper');
 const errmsg_scan = '이러닝이 아닙니다😢';
 const errmsg_vid = '강의가 아닙니다😢';
 
-scanBt.addEventListener('click', () => {
+document.querySelector('#help_bt').addEventListener('click', () => {
+  window.open('/howtouse/index.html');
+});
+
+scanOneBt.addEventListener('click', () => {
   chrome.tabs.executeScript(
     {
       code:
@@ -15,6 +20,35 @@ scanBt.addEventListener('click', () => {
         view.innerHTML += `<div style="margin-top:5px; color: crimson;">${errmsg_scan}</div>`;
       } else {
         window.open(clmsUrl[0]);
+      }
+    },
+  );
+});
+
+scanTwoBt.addEventListener('click', () => {
+  chrome.tabs.executeScript(
+    {
+      code:
+        'document.getElementsByTagName("iframe")[1].contentDocument.getElementsByTagName("iframe")[0].contentDocument.getElementsByTagName("iframe")[0].src',
+    },
+    (clmsUrl1) => {
+      if (chrome.runtime.lastError || clmsUrl1[0].length < 10) {
+        view.innerHTML += `<div style="margin-top:5px; color: crimson;">${errmsg_scan}</div>`;
+      } else {
+        view.innerHTML += `<a href="${clmsUrl1}" target="_blank">첫번째 강의 열기</a>`;
+        chrome.tabs.executeScript(
+          {
+            code:
+              'document.getElementsByTagName("iframe")[1].contentDocument.getElementsByTagName("iframe")[1].contentDocument.getElementsByTagName("iframe")[0].src',
+          },
+          (clmsUrl2) => {
+            if (chrome.runtime.lastError || clmsUrl2[0].length < 10) {
+              view.innerHTML += `<div style="margin-top:5px; color: crimson;">${errmsg_scan}</div>`;
+            } else {
+              view.innerHTML += `<a href="${clmsUrl2}" target="_blank">두번째 강의 열기</a>`;
+            }
+          },
+        );
       }
     },
   );
@@ -41,8 +75,4 @@ videoBt.addEventListener('click', () => {
       }
     },
   );
-});
-
-document.querySelector('#help_bt').addEventListener('click', () => {
-  window.open('/howtouse/index.html');
 });
